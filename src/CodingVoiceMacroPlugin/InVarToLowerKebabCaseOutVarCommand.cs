7de0c01b98fe54acf965f86e1daaeeba79faf798
Seth Hendrick <seth@shendrick.net>
2021-08-29 15:24:46 -0400

    Added more casing types.

---
+++ src/CodingVoiceMacroPlugin/InVarToLowerKebabCaseOutVarCommand.cs
@@ -0,0 +1,37 @@
+﻿//
+//          Copyright Seth Hendrick 2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using SethCS.Extensions;
+
+namespace VoiceMacro.Plugins.Coding
+{
+    public sealed class InVarToLowerKebabCaseOutVarCommand : BaseInVarToCaseOutVarCommand
+    {
+        // ---------------- Properties ----------------
+
+        public override string DisplayName => "Input Var, To Lower Kebab Case, Output var";
+
+        public override string Description => 
+$@"This command takes in the name of a variable, transforms its value to Lower Kebab Case, and outputs the value into a different variable.
+For example, 'Hello world how are uou' becomes 'hello-world-how-are-you'.
+    Argument 1 - The name of the variable to read the value from {Constants.VariableTypeDescription}.
+    Argument 2 - The name of the variable to save the transformed value to {Constants.VariableTypeDescription}.
+                 The variable will contain {Constants.ErrorString} if the variable in argument 1 does not exist.
+    Argument 3 - Not used.
+    This command is always Synchronized
+";
+
+        public override string ID => "532A4F03-02F3-4D88-B718-CAB8CDA9A0D9";
+
+        // ---------------- Functions ----------------
+
+        protected override string Transform( string input )
+        {
+            return input.ToLowerKebabCase();
+        }
+    }
+}
---
+++ src/CodingVoiceMacroPlugin/InVarToUpperKebabCaseOutVarCommand.cs
@@ -0,0 +1,37 @@
+﻿//
+//          Copyright Seth Hendrick 2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using SethCS.Extensions;
+
+namespace VoiceMacro.Plugins.Coding
+{
+    public sealed class InVarToUpperKeabCaseOutVarCommand : BaseInVarToCaseOutVarCommand
+    {
+        // ---------------- Properties ----------------
+
+        public override string DisplayName => "Input Var, To Upper Kebab Case, Output var";
+
+        public override string Description => 
+$@"This command takes in the name of a variable, transforms its value to Upper Kebab Case, and outputs the value into a different variable.
+For example, 'Hello world how are uou' becomes 'HELLO-WORLD-HOW-ARE-YOU'.
+    Argument 1 - The name of the variable to read the value from {Constants.VariableTypeDescription}.
+    Argument 2 - The name of the variable to save the transformed value to {Constants.VariableTypeDescription}.
+                 The variable will contain {Constants.ErrorString} if the variable in argument 1 does not exist.
+    Argument 3 - Not used.
+    This command is always Synchronized
+";
+
+        public override string ID => "E912F621-EC3F-4853-8C09-BD4C2C5F5A29";
+
+        // ---------------- Functions ----------------
+
+        protected override string Transform( string input )
+        {
+            return input.ToUpperKebabCase();
+        }
+    }
+}
