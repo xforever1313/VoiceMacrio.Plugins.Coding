0b7891974e1287bc882a8ac249623709b136f2d2
Seth Hendrick <seth@shendrick.net>
2021-08-29 21:54:11 -0400

    Added plugin source URL and plugin version commands

---
+++ src/VoiceMacro.Plugins.Coding/GetSourceUrlOutVarCommand.cs
@@ -0,0 +1,51 @@
+﻿//
+//          Copyright Seth Hendrick 2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using vmAPI;
+
+namespace VoiceMacro.Plugins.Coding
+{
+    public sealed class GetSourceUrlOutVarCommand : vmInterface
+    {
+        // ---------------- Fields ----------------
+
+        private const string url = "https://github.com/xforever1313/VoiceMacro.Plugins.Coding";
+
+        // ---------------- Properties ----------------
+
+        public string DisplayName => $"{Constants.DisplayNamePrefix} Get Source URL Version, Output Var";
+
+        public string Description =>
+$@"Gets this plugin's URL to the source code and outputs it to the specified variable.
+    Argument 1 - The name of the variable to write the URL to {Constants.VariableTypeDescription}.
+    Argument 2 - Not Useed
+    Argument 3 - Not used.
+    This command is always Synchronized
+";
+
+        public string ID => "421B4352-5F80-4838-BE37-668B3EBED5E3";
+
+        // ---------------- Functions ----------------
+
+        public void Init()
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public void ProfileSwitched( string ProfileGUID, string ProfileName )
+        {
+        }
+
+        public void ReceiveParams( string Param1, string Param2, string Param3, bool Synchron )
+        {
+            vmCommand.SetVariable( Param1, url );
+        }
+    }
+}
---
+++ src/VoiceMacro.Plugins.Coding/GetVersionOutVarCommand.cs
@@ -0,0 +1,50 @@
+﻿//
+//          Copyright Seth Hendrick 2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System;
+using System.Reflection;
+using vmAPI;
+
+namespace VoiceMacro.Plugins.Coding
+{
+    public sealed class GetVersionOutVarCommand : vmInterface
+    {
+        // ---------------- Properties ----------------
+
+        public string DisplayName => $"{Constants.DisplayNamePrefix} Get Plugin Version, Output Var";
+
+        public string Description =>
+$@"Gets this plugin's version and outputs it to the specified variable.
+    Argument 1 - The name of the variable to write the version to {Constants.VariableTypeDescription}.
+    Argument 2 - Not Useed
+    Argument 3 - Not used.
+    This command is always Synchronized
+";
+
+        public string ID => "811CA919-C5BE-46D6-960E-CA76421D2018";
+
+        // ---------------- Functions ----------------
+
+        public void Init()
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public void ProfileSwitched( string ProfileGUID, string ProfileName )
+        {
+        }
+
+        public void ReceiveParams( string Param1, string Param2, string Param3, bool Synchron )
+        {
+            Version vers = typeof( GetVersionOutVarCommand ).GetTypeInfo().Assembly.GetName().Version;
+            vmCommand.SetVariable( Param1, vers.ToString( 3 ) );
+        }
+    }
+}
